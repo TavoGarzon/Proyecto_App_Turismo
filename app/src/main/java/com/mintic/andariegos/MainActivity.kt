fea6556f5dc9539d24c6849c8bce7beffc7ffe5f
crisrodcas <cristian.rodcas95@gmail.com>
2021-11-14 18:51:50 -0500

    activity santurban

--- app/src/main/java/com/mintic/andariegos/MainActivity.kt
+++ app/src/main/java/com/mintic/andariegos/MainActivity.kt
@@ -1,9 +1,7 @@
 package com.mintic.andariegos
 
-import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,16 +9,11 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-         var botoom = findViewById<Button>(R.id.boton1)
-        botoom.setOnClickListener {
-          var  intent = Intent(this,chingaza::class.java )
+        setContentView(R.layout.activity_santurban)
           startActivity(intent)
         }
 
-
-       }
-
-
-
 }
+
+
+
