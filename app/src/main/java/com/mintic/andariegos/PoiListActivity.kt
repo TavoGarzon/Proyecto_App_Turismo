42949f1a496feca91c189c7f13eca93625f7a233
crisrodcas <cristian.rodcas95@gmail.com>
2021-11-20 13:04:40 -0500

    layout Poi List Activity

---
+++ app/src/main/java/com/mintic/andariegos/PoiListActivity.kt
@@ -0,0 +1,11 @@
+package com.mintic.andariegos
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class PoiListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_poi_list)
+    }
+}
