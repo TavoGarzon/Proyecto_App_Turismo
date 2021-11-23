85a4699d3a426927e0b2824c9d6f07037fdedfe2
crisrodcas <cristian.rodcas95@gmail.com>
2021-11-23 02:33:40 -0500

    solucion de conflictos

--- app/src/main/java/com/mintic/andariegos/chingaza.kt
+++ app/src/main/java/com/mintic/andariegos/chingaza.kt
@@ -1,20 +1,14 @@
 package com.mintic.andariegos
 
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class chingaza : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chingaza)
-
-        var boton = findViewById<Button>(R.id.retornar)
-        boton.setOnClickListener {
-            var intent = Intent(this,sitios::class.java)
-            startActivity(intent)
-        }
 
     }
 }
