0683901dcb8a6a5f937cd911244c6f8572b4167c
crisrodcas <cristian.rodcas95@gmail.com>
2021-11-23 01:07:18 -0500

    Recycler View con lista

--- app/src/main/java/com/mintic/andariegos/PoiListActivity.kt
+++ app/src/main/java/com/mintic/andariegos/PoiListActivity.kt
@@ -2,7 +2,7 @@ package com.mintic.andariegos
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class PoiListActivity : AppCompatActivity() {
@@ -21,12 +21,18 @@ class PoiListActivity : AppCompatActivity() {
 
         sitiosAdapter = SitiosAdapter(listSitios)
 
-        poiRecyclerView.addItemDecoration(DividerItemDecoration(this, DividerItemDecoration.VERTICAL))
-        poiRecyclerView.adapter = sitiosAdapter
+        /*poiRecyclerView.addItemDecoration(DividerItemDecoration(this, DividerItemDecoration.VERTICAL))*/
+
+        poiRecyclerView.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = sitiosAdapter
+            setHasFixedSize(false)
+        }
+        /*poiRecyclerView.adapter = sitiosAdapter*/
     }
 
     private fun createMockSitios(): ArrayList<Sitio> {
-        var lista: ArrayList<Sitio> = arrayListOf()
+        /*var lista: ArrayList<Sitio> = arrayListOf()
         var sitio = Sitio(nombre = "Paramo de Santurbán",
                 descripcion = "El Páramo de Santurbán se ubica en los departamentos de Santander y Norte de Santander en una altura desde los 2.800 y hasta los 4.290 metros sobre el nivel del mar." +
                         "El Páramo de Santurbán es hábitat de 457 especies de plantas, 17 de anfibios, 17 de reptiles, 201 de aves y 58 de mamíferos." +
@@ -35,15 +41,16 @@ class PoiListActivity : AppCompatActivity() {
                 puntuacion = "8.7/10"
                 )
         lista.add(sitio)
+        return list*/
 
         return arrayListOf(
                 Sitio(
                         nombre = "Paramo de Santurbán",
-                        descripcion = "El Páramo de Santurbán se ubica en los departamentos de Santander y Norte de Santander en una altura desde los 2.800 y hasta los 4.290 metros sobre el nivel del mar." +
-                                "El Páramo de Santurbán es hábitat de 457 especies de plantas, 17 de anfibios, 17 de reptiles, 201 de aves y 58 de mamíferos." +
-                                "Si te gusta la fotografía encontrarás paisajes dignos de postales y si disfrutas el ecoturismo estar rodeado de tanta naturaleza mientras el frío rosa tu piel te dejará fascinado.",
-                        caracteristicas = "Temperatura: 0°c hasta 32°c" + "Sitios recomendados: Los mayores atractivos de turismo en el Páramo Santurbán son sus Lagunas.",
-                        puntuacion = "8.7/10")
+                        descripcion = "El Páramo de Santurbán se ubica en los departamentos de Santander y Norte de Santander en una altura desde los 2.800 y hasta los 4.290 metros sobre el nivel del mar. " +
+                                "Es hábitat de 457 especies de plantas, 17 de anfibios, 17 de reptiles, 201 de aves y 58 de mamíferos. " +
+                                "Los mayores atractivos de turismo en el Páramo Santurbán son sus Lagunas.",
+                        caracteristicas = "Temperatura: 0°c hasta 32°c. ",
+                        puntuacion = "Puntuación: 8.7/10")
         )
     }
 }
