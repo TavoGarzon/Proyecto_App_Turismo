045cf7d6da1f7f84d3487366fc073791621dcdf7
crisrodcas <cristian.rodcas95@gmail.com>
2021-11-23 01:39:04 -0500

    Recycler View con lista 3 sitios

--- app/src/main/java/com/mintic/andariegos/PoiListActivity.kt
+++ app/src/main/java/com/mintic/andariegos/PoiListActivity.kt
@@ -50,7 +50,19 @@ class PoiListActivity : AppCompatActivity() {
                                 "Es hábitat de 457 especies de plantas, 17 de anfibios, 17 de reptiles, 201 de aves y 58 de mamíferos. " +
                                 "Los mayores atractivos de turismo en el Páramo Santurbán son sus Lagunas.",
                         caracteristicas = "Temperatura: 0°c hasta 32°c. ",
-                        puntuacion = "Puntuación: 8.7/10")
+                        puntuacion = "Puntuación: 8.7/10"),
+                Sitio(
+                        nombre = "Laguna del Otún",
+                        descripcion = "Es un embalse natural que se encuentra en el departamento de Risaralda, en el área perteneciente al Parque Nacional Natural Los Nevados. ",
+                        caracteristicas = "Está formada por la acción volcánica y glaciar alimentada principalmente por el deshielo del Nevado de Santa Isabel.\n" +
+                                "Temperatura promedio: 10°C\n",
+                        puntuacion = "Puntuación: 8.4/10"),
+                Sitio(
+                        nombre = "Cocora",
+                        descripcion = "En la reserva natural Valle del Cocora se encuentra el hogar del árbol nacional de Colombia, la Palma de Cera del Quindío. una especie nativa del bosque andino colombiano. "+
+                                "Se encuentra dentro de los límites de Parque Nacional de los Nevados, uno de los Parques más importantes de la región, pues es el nacimiento de decenas de ríos que surten de agua la región central del país. ",
+                        caracteristicas = "Su temperatura oscila entre los 5ºC y 16ºC.",
+                        puntuacion = "Puntuación: 8.5/10")
         )
     }
 }
